18110321721c397d8fef3215b62ac6141c7a799f
Matthew Brown <mlbrown1@aep.com>
2019-05-10 20:18:57 -0400

    Added foundation.scss

--- src/app/fetch.service.ts
+++ src/app/fetch.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TriviaCategory } from './trivia-category';
+import { Observable } from 'rxjs';
+import { SessionToken } from './session-token';
 
-@Injectable({
-  providedIn: 'root'
-})
+const categoriesUrl = 'https://opentdb.com/api_category.php';
+const tokenRequestUrl = 'https://opentdb.com/api_token.php?command=request';
 export class FetchService {
-
   constructor(private http: HttpClient) { }
 
-  getCategoryList() {
+  getCategoryList(): Observable<TriviaCategory[]> {
+    return this.http.get<TriviaCategory[]>(categoriesUrl);
+  }
 
+  getToken(): Observable<SessionToken> {
+    return this.http.get<SessionToken>(tokenRequestUrl);
   }
 }
--- src/app/session-token.ts
+++ src/app/session-token.ts
@@ -1,2 +1,5 @@
 export interface SessionToken {
+  response_code: number;
+  response_message: string;
+  token: string;
 }
