ef0b2a44b0d110404b248bd2e400f581c7c6e9d3
Matthew Brown <mlbrown1@aep.com>
2019-05-13 11:15:45 -0400

    Additional styling, added nes.css

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class NavComponent implements OnInit {
-  name = 'dive bar trivia';
+  nameLabel = 'Dive Bar Trivia';
+  homeLabel = 'Home';
+  aboutLabel = 'About';
+  newGameLabel = 'Start New Game';
   constructor() { }
 
   ngOnInit() {
